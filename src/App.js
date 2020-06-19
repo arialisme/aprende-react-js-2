58299941cebba70a84766ffe28c78c7e54d9091f
Elaimy <arialisme@gmail.com>
2020-06-19 02:20:34 -0300

    Tercera forma de crear componente en una sola linea

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function Hello(props) {
-  return <h2>{props.title}</h2>;
-}
+// function Hello(props) {
+//  return <h2>{props.title}</h2>;
+//}
+
+const Hello = (props) => <h2>{props.title}</h2>;
 
 function App() {
   return (
