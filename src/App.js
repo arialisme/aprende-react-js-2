d4abc03f8839a849d6e3ade1698560a42ffd9f0b
Elaimy <arialisme@gmail.com>
2020-06-18 21:59:32 -0300

    Creamos un Hola Mundo en React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Bienvenidos a este curso</h2>
+        <h3>Vamos a aprender React</h3>
+        <p>Hola mundo, estoy usando React</p>
+        <strong>Esto es un texto strong</strong>
       </header>
     </div>
   );
