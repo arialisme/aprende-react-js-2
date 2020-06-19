0e4ddf6ca883a384fb5fadd294c30919b247db2e
Elaimy <arialisme@gmail.com>
2020-06-19 02:08:17 -0300

    Primera forma de crear un componente

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+function Hello(props) {
+  return <h2>Bienvenidos a este curso</h2>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Bienvenidos a este curso</h2>
-        <h3>Vamos a aprender React</h3>
-        <p>Hola mundo, estoy usando React</p>
-        <strong>Esto es un texto strong</strong>
+        <Hello />
       </header>
     </div>
   );
