1203305b70e50c95d664c7af4df8e1d4a63e481c
Elaimy <arialisme@gmail.com>
2020-06-19 22:24:23 -0300

    Profundización de la propagación del estado e inicialización con props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 
 class Contador extends Component {
   constructor(props) {
     super(props);
-    console.log(this.props.contadorInicial);
+    //console.log(this.props.contadorInicial);
+    console.log("Entre en constructor de Contador");
     this.state = { contador: this.props.contadorInicial };
-    setInterval(() => {
-      this.setState({ contador: this.state.contador + 1 });
-    }, 1000);
   }
-
   render() {
+    console.log("renderizado de Contador");
     return <ContadorNumero numero={this.state.contador} />;
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      console.log("Ya esta montado  Contador");
+      var id = setInterval(() => {
+        this.setState({ contador: this.state.contador + 1 });
+        if (this.state.contador === 3) clearInterval(id);
+      }, 1000);
+    }, 5000);
   }
 }
 
 Contador.defaultProps = { contadorInicial: 0 };
 
 class ContadorNumero extends Component {
+  constructor(props) {
+    super(props);
+    //console.log(this.props.contadorInicial);
+    console.log("Entre en constructor de ContadorNumero");
+  }
   render() {
-    console.log("ContadorNumero()");
+    console.log("renderizado de ContadorNumero");
     return <span>{this.props.numero}</span>;
   }
 }
 class App extends Component {
   render() {
+    console.log("renderizado de App");
     return (
       <div className="App">
         <p>Primer componente con state</p>
-        <Contador contadorInicial={100} />
+        <Contador contadorInicial={1} />
       </div>
     );
   }
