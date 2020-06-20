cc07a5fd1a6f5531252b107c79f3551229cc7df4
Elaimy <arialisme@gmail.com>
2020-06-20 00:08:40 -0300

    Listas de Objetos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
+import cars from "./data/cars.json";
 import "./App.css";
+
+class CarItem extends Component {
+  render() {
+    const { car, id } = this.props;
+
+    return (
+      <li>
+        <p>
+          <strong>Key:</strong> {id}
+        </p>
+        <p>
+          <strong>Nombre: </strong>
+          {car.name}
+        </p>
+        <p>
+          <strong>Marca: </strong>
+          {car.company}
+        </p>
+      </li>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    const numbers = [1, 1, 3, 4, 5];
     return (
       <div className="App">
-        <h4>Trabajando con las Listas</h4>
-        {numbers.map((number, index) => {
-          return <p key={index}>Soy el numero {number}</p>;
-        })}
+        <h4>Trabajando Listas con Objetos</h4>
+        <ul>
+          {cars.map((car) => {
+            return <CarItem id={car.id} key={car.id} car={car} />;
+          })}
+        </ul>
       </div>
     );
   }
