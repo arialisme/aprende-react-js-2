ccde476d480ee9b8528bec1bf6747e378350afd6
Elaimy <arialisme@gmail.com>
2020-06-20 21:41:59 -0300

    Particularidades del atributo For

--- src/sections/forms.js
+++ src/sections/forms.js
@@ -15,7 +15,7 @@ export default class Forms extends Component {
         <h4>Formularios</h4>
         <form>
           <p>
-            <label>Nombre: </label>
+            <label htmlFor="name">Nombre: </label>
             <input
               id="name"
               name="userName"
@@ -23,7 +23,7 @@ export default class Forms extends Component {
             />
           </p>
           <p>
-            <label>Twitter:</label>
+            <label htmlFor="twitter">Twitter:</label>
             <input
               id="twitter"
               name="twitterAccount"
