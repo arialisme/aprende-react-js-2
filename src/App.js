8b1982043a26c0ced6ad651947789ac0951655c2
Elaimy <arialisme@gmail.com>
2020-06-22 17:27:37 -0300

    El Constructor - Primera Fase del Ciclo de Montaje de nuestros Componentes

--- src/App.js
+++ src/App.js
@@ -1,85 +1,36 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import "./App.css";
 
-class Box extends Component {
-  render() {
-    console.log("Estoy en el render del componente Box");
-    return (
-      <div style={{ border: "1px solid #09F", margin: 5, padding: 5 }}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-class Article extends Component {
-  static propTypes = { author: PropTypes.string.isRequired };
-
-  render() {
-    console.log("Estoy en el componente Article");
-    const { author, children, date, title } = this.props;
-    return (
-      <section>
-        <h2>{title}</h2>
-        {author && (
-          <p>
-            <em>Escrito por {author}</em>
-          </p>
-        )}
-        <Box>{date}</Box>
-        <article>{children}</article>
-      </section>
-    );
-  }
-}
-
 class App extends Component {
+  /* Constructor por defecto
+  constructor(...args) {
+    super(...args);
+  }
+*/
+
+  constructor(props) {
+    console.log("constructor");
+    super(props); //Este método llama al constructor de Component
+    //Inicializamos el state de nuestro componente, se le asigna un objeto a la propiedad this.state
+    this.state = { mensajeInicial: "Mensaje Inicial " };
+    /*Bindeamos el contexto al método, mediante la propiedad bind que devuelve el mismo método con el contexto que le 
+    hemos pasado por parámetro enlazado a la ejecución de la función  
+    this.handleClick = this.handleClick.bind(this); */
+  }
+
+  // = () => Usar Arrow function es más simple que usar el método bind
+  handleClick = () => {
+    console.log("handleClick");
+    this.setState({ mensajeInicial: "Mensaje cambiado" });
+  };
+
   render() {
-    console.log("Estoy en el render del componente App");
+    console.log("render");
     return (
       <div className="App">
-        <h4>Children props</h4>
-        <Article
-          date={new Date().toLocaleDateString()}
-          title="Artículo sobre la prop children"
-        >
-          <p>
-            El contenido que envolvemos dentro del Componente Article será
-            enviado al componente como this.props.children.
-          </p>
-          <strong>
-            Y mantiene las etiquetas y componentes que hayas añadido dentro.
-          </strong>
-        </Article>
-
-        <Article
-          author="Gabriel"
-          date={new Date().toLocaleDateString()}
-          title="Artículo 2"
-        >
-          <p>
-            El contenido que envolvemos dentro del Componente Article será
-            enviado al componente como this.props.children.
-          </p>
-          <strong>
-            Y mantiene las etiquetas y componentes que hayas añadido dentro.
-          </strong>
-        </Article>
-
-        <Article
-          author="Elaimy Arias Lismé"
-          date={new Date().toLocaleDateString()}
-          title="Otro Artículo"
-        >
-          <p>
-            El contenido que envolvemos dentro del Componente Article será
-            enviado al componente como this.props.children.
-          </p>
-          <strong>
-            Y mantiene las etiquetas y componentes que hayas añadido dentro.
-          </strong>
-        </Article>
+        <h4>Ciclo de Montaje: Constructor</h4>
+        {this.state.mensajeInicial}
+        <button onClick={this.handleClick}>Cambiar Mensaje</button>
       </div>
     );
   }
