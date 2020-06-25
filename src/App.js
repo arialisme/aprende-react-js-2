8076f6f5f9317efc37f0c00d8847a939dd7fac86
Elaimy <arialisme@gmail.com>
2020-06-25 18:35:15 -0300

    Refactor de ComponentWillReceiveProps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import EjemploDeCicloDeActualizacion from "./sections/life-cycle/ejemploCicloDeActualizacion";
+import RefactorDeEjemploDeCicloDeActualizacion from "./sections/life-cycle/refactorDeEjemploCicloDeActualizacion";
 import "./App.css";
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
     console.log("render del componente App");
     return (
       <div className="App">
-        <EjemploDeCicloDeActualizacion />
+        <RefactorDeEjemploDeCicloDeActualizacion />
       </div>
     );
   }
--- src/sections/life-cycle/refactorDeEjemploCicloDeActualizacion.js
+++ src/sections/life-cycle/refactorDeEjemploCicloDeActualizacion.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 const ANIMAL_IMAGES = {
+  panda: "https://goo.gl/oNbtoq",
   cat: "https://goo.gl/PoQQXb",
   dolphin: "https://goo.gl/BbiKCd",
-  panda: "https://goo.gl/oNbtoq",
 };
+
+const ANIMALS = Object.keys(ANIMAL_IMAGES);
 
 class AnimalImage extends Component {
   state = { src: ANIMAL_IMAGES[this.props.animal] };
 
   componentWillReceiveProps(nextProps) {
-    /*nextProps pueden ser las mismas props que tenemos pero entrará igualmete a este método. 
-      Nuevas props no requieren decir que son distintas a las que teníamos */
     console.log("componentWillReceiveProps");
     console.log(nextProps);
     this.setState({ src: ANIMAL_IMAGES[nextProps.animal] });
@@ -30,30 +30,34 @@ class AnimalImage extends Component {
 }
 
 AnimalImage.propTypes = {
-  animal: PropTypes.oneOf(["cat", "dolphin", "panda"]),
+  animal: PropTypes.oneOf(ANIMALS),
 };
 
-AnimalImage.defaultProps = { animal: "panda" };
-
-class EjemploDeCicloDeActualizacion extends Component {
+class RefactorDeEjemploDeCicloDeActualizacion extends Component {
   state = { animal: "panda" };
+
+  _renderAnimalButton = (animal) => {
+    return (
+      <button
+        disabled={animal === this.state.animal}
+        key={animal}
+        onClick={() => this.setState({ animal })}
+      >
+        {animal}
+      </button>
+    );
+  };
 
   render() {
     console.log("render del componente EjemploDeCicloDeActualizacion");
     return (
       <div>
         <h4>Ciclo de Actualización, Ejemplo de: ComponentWillReceiveProps </h4>
-        <button onClick={() => this.setState({ animal: "panda" })}>
-          Panda
-        </button>
-        <button onClick={() => this.setState({ animal: "cat" })}>Cat</button>
-        <button onClick={() => this.setState({ animal: "dolphin" })}>
-          Dolphin
-        </button>
+        {ANIMALS.map(this._renderAnimalButton)}
         <AnimalImage animal={this.state.animal} />
       </div>
     );
   }
 }
 
-export default EjemploDeCicloDeActualizacion;
+export default RefactorDeEjemploDeCicloDeActualizacion;
