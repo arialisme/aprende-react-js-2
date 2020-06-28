80466009324246f2d12ab9fb063fa1ca105c2c26
Elaimy <arialisme@gmail.com>
2020-06-28 17:46:40 -0300

    ComponentDidUpdate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import RefactorDeEjemploDeCicloDeActualizacion from "./sections/life-cycle/componentWillUpddate";
+import RefactorDeEjemploDeCicloDeActualizacion from "./sections/life-cycle/componentDidUpddate";
 import "./App.css";
 
 class App extends Component {
